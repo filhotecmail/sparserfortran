685864e6f577972c874a59493a1f072f9cbdc041
Carlos Alberto Dias da Silva Filho <filhotecmail@gmail.com>
2025-01-17 13:44:47 -0300

    [update]

---
+++ target/debug/deps/adler-6e2239da07b4bf7a.d
@@ -0,0 +1,8 @@
+/home/cads/sparserfortran/sparserfortran/target/debug/deps/libadler-6e2239da07b4bf7a.rmeta: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/src/lib.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/src/algo.rs
+
+/home/cads/sparserfortran/sparserfortran/target/debug/deps/libadler-6e2239da07b4bf7a.rlib: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/src/lib.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/src/algo.rs
+
+/home/cads/sparserfortran/sparserfortran/target/debug/deps/adler-6e2239da07b4bf7a.d: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/src/lib.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/src/algo.rs
+
+/home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/src/lib.rs:
+/home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/adler-1.0.2/src/algo.rs:
---
+++ target/debug/deps/iana_time_zone-c322e5d8bb651c78.d
@@ -0,0 +1,9 @@
+/home/cads/sparserfortran/sparserfortran/target/debug/deps/libiana_time_zone-c322e5d8bb651c78.rmeta: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/lib.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/ffi_utils.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/tz_linux.rs
+
+/home/cads/sparserfortran/sparserfortran/target/debug/deps/libiana_time_zone-c322e5d8bb651c78.rlib: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/lib.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/ffi_utils.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/tz_linux.rs
+
+/home/cads/sparserfortran/sparserfortran/target/debug/deps/iana_time_zone-c322e5d8bb651c78.d: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/lib.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/ffi_utils.rs /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/tz_linux.rs
+
+/home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/lib.rs:
+/home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/ffi_utils.rs:
+/home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/iana-time-zone-0.1.61/src/tz_linux.rs:
