6314d653e245dd450cba9c62525c3b4537a26ab4
Carlos Alberto Dias da Silva Filho <filhotecmail@gmail.com>
2025-01-17 13:45:43 -0300

    [update]

---
+++ target/debug/build/hashbrown-08c884fd27dc3534/build_script_build-08c884fd27dc3534.d
@@ -0,0 +1,5 @@
+/home/cads/sparserfortran/sparserfortran/target/debug/build/hashbrown-08c884fd27dc3534/build_script_build-08c884fd27dc3534: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hashbrown-0.8.2/build.rs
+
+/home/cads/sparserfortran/sparserfortran/target/debug/build/hashbrown-08c884fd27dc3534/build_script_build-08c884fd27dc3534.d: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hashbrown-0.8.2/build.rs
+
+/home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/hashbrown-0.8.2/build.rs:
---
+++ target/debug/build/num-traits-4ca75d1196e04c70/build_script_build-4ca75d1196e04c70.d
@@ -0,0 +1,5 @@
+/home/cads/sparserfortran/sparserfortran/target/debug/build/num-traits-4ca75d1196e04c70/build_script_build-4ca75d1196e04c70: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
+
+/home/cads/sparserfortran/sparserfortran/target/debug/build/num-traits-4ca75d1196e04c70/build_script_build-4ca75d1196e04c70.d: /home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs
+
+/home/cads/.cargo/registry/src/index.crates.io-6f17d22bba15001f/num-traits-0.2.19/build.rs:
